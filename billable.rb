5b68a2d69a9978ad5c37b4e5c1f6272f6c2df3fa
Julien Lamarche <julien.lamarche@gmail.com>
2016-10-13 08:41:57 -0400

    added  logon.rb billable.rb

---
+++ billable.rb
@@ -0,0 +1,37 @@
+require_relative 'session'
+
+class Logon < Session
+
+    def initialize()
+
+    end
+
+    def self.readDir(path)
+        if ! Dir.exist?(File.expand_path(path))
+            return nil
+        end
+
+        returnArray = []
+        Dir[File.expand_path(path) + '/*.csv'].each { |f|
+            doc = Nokogiri::HTML(File.expand_path(File.read(f)))
+
+            rows = doc.xpath('//tr')
+
+            rows.each { |r|
+                s = Session.new
+                interval = s.setDateTimes(r.children[0], r.children[1])
+
+                if interval === FALSE
+                    next
+                else
+                    returnArray.push(s)
+                end
+            }
+
+        }
+
+        return returnArray
+    end
+
+end
+
