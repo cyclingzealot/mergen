8fb46e528a0b4948a485f04554d66045413c6915
Julien Lamarche <julien.lamarche@gmail.com>
2016-10-15 16:09:41 -0400

    WIP, adding assocation of billables to logons, marking logons that have 0 billables

--- billable.rb
+++ billable.rb
@@ -6,7 +6,7 @@ class Billable < Session
         super("%Y-%m-%d %H:%M:%S")
     end
 
-    def self.readDir(path)
+    def self.readDir(path, logons)
         if ! Dir.exist?(File.expand_path(path))
             return nil
         end
@@ -14,12 +14,17 @@ class Billable < Session
         returnArray = []
         Dir[File.expand_path(path) + '/*.csv'].each { |f|
 
+            minDateTimeInFile = 0
+            maxDateTimeInFile = DateTime.new('2038-01-19 04:14:07')
             CSV.parse(File.read(f)) { |l|
                 b = Billable.new
 
                 b.setDateTimes(l[0], l[1])
 
                 returnArray.push(b)
+            }
+
+            logons.each { |l|
             }
 
         }
