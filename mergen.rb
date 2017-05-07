c3aef494c32993231b91b6a66e74e790dba857bc
Julien Lamarche <julien.lamarche@gmail.com>
2017-05-06 20:28:48 -0400

    added capacity to produce by hour weekly calendar

--- mergen.rb
+++ mergen.rb
@@ -8,6 +8,7 @@ require_relative 'logon'
 require_relative 'billable'
 require_relative 'session'
 require_relative 'stat'
+require_relative 'weekCalendar'
 
 loginHoursPerMonth = []
 
@@ -28,11 +29,12 @@ end
 
 logonSessions = Logon.readDir(sessionDataPath)
 billableSessions = Billable.readDir(sessionDataPath, logonSessions)
-pctBusySessions = {}
 
-[Stat::BY_DAYOFWEEK, Stat::BY_HOUROFWEEK].each {|period|
-	billedByPeriod = Session.byPeriodTotals(billableSessions, period, TRUE)
-	logonByPeriod = Session.byPeriodTotals(logonSessions, period, TRUE)
+[Stat::BY_DAYOFWEEK, Stat::BY_HOUROFWEEK].each {|periodType|
+    pctBusySessions = {}
+
+	billedByPeriod = Session.byPeriodTotals(billableSessions, periodType, TRUE)
+	logonByPeriod = Session.byPeriodTotals(logonSessions, periodType, TRUE)
 
 	billedByPeriod.each { |k,v|
 	    logonTotal = logonByPeriod[k]
@@ -46,9 +48,29 @@ pctBusySessions = {}
 	    puts k.to_s + ': ' + '%.2f %% (%.2f / %.2f) ' % [pctBusy, v, logonTotal]
 	}
 
-    puts
+    pctBusySessions = pctBusySessions.sort_by {|k,v| v}.reverse
+
+    pctBusySessions.each { |k, pctBusy|
+        puts k.to_s + ': ' + '%.2f' % pctBusy
+    }
+
+    if periodType == Stat::BY_HOUROFWEEK
+        require 'money'
+        require 'money/bank/google_currency'
+        Money::Bank::GoogleCurrency.ttl_in_seconds = 86400
+        Money.default_bank = Money::Bank::GoogleCurrency.new
+        dollarsPerMin = (Money.new(55, "USD").exchange_to(:CAD).cents.to_f/100)
+        lowTarget = 500/dollarsPerMin/60
+        midTarget = 1000/dollarsPerMin/60
+        highTarget = 1200/dollarsPerMin/60
+        wc = WeekCalendar.new(lowTarget, midTarget, highTarget, periodType)
+        wc.setTargetValuesForPeriods(pctBusySessions)
+        wc.generateData
+
+        filePath = '/tmp/mergen.' + periodType + '.html'
+        IO.write(filePath, wc.generateHourOfWeekHTML.to_s)
+        $stderr.puts "See #{filePath}.  May want to use firefox #{filePath} & ."
+    end
 }
 
-pctBusySessions.sort_by {|k,v| v}.reverse.each { |k, pctBusy|
-    puts k.to_s + ': ' + '%.2f' % pctBusy
-}
+
