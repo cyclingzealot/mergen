18a04b885ec3d3e8d68884dcf2017615e3a466f6
Julien Lamarche <julien.lamarche@gmail.com>
2018-08-03 09:54:00 -0400

    added rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mergen::Application.config.secret_key_base = '352f9ffad4649c7dc14fbab936a158044ebb47d21c60329eeed6e1a248f9b4f7272c2e72d503348e89f182240fd449044b22b4208f80d15f40201b265ab23643'
